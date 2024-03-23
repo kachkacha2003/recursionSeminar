89b0159cc765837d3e31d66aca00f0168d04a9ab
lasha <lasha.katchkatchishvili.1@btu.edu.ge>
2024-03-23 07:24:09 +0400

    recursionSeminar

---
+++ script.js
@@ -0,0 +1,89 @@
+// function foo1(){
+//     console.log("one");
+//     foo2();
+//     console.log("one");
+
+// }
+
+// function foo2(){
+//     console.log("two")
+//     foo3();
+//     console.log("two")
+// }
+
+// function foo3(){
+//     console.log("three");
+// }
+
+// foo1();
+
+// one,(foo2),two,(foo3),three,two,one
+
+
+// for(let i=0;i<3;i++){
+//     console.log("hello world");
+// }
+// let count=0;
+// let count=0;
+
+// function foo2(){
+//     return 4;
+// }
+// function foo(){
+  
+   
+//     count++;
+//     if(foo2()==count){
+//         console.log("bitcamp");
+//         return ;
+//     };
+//     console.log("hello world");
+//     foo();
+// }
+
+// foo();
+
+
+
+// function factorial(num){
+    
+//     if(num==0 || num==1){
+//         return 1;
+//     }
+//     return num * factorial(num-1);
+// }
+
+// console.log(factorial(5));
+
+
+// let count=0;
+
+// for(let i=1;i<6;i++){
+//     if(i==1){
+//         count=count+1 *i;
+//     }else{
+//         count=count *i;
+//     }
+   
+// }
+// console.log(count)
+
+// 5 *factorial(4)
+// factorial(4)=4 * factorial(3)
+
+//5!=5 *4 *3 *2 *1
+// 5!=5 *4 * 3 * 2!
+
+
+// 5!=5 * 4!
+// 4!=4 * 3!
+// 3!=3 *2!
+// 2!=2* 1!
+// 1!=1
+
+
+// 5!=5 * 24
+// 4!=4 * 6
+// 3!=3 *2
+// 2!=2* 1     2 * (1)
+// 1!=1
